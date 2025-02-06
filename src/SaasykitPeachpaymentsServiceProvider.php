49f73b399589029dc1ac5ac36272246b423602f5
Fabio <fabio@mymage.io>
2025-02-06 19:09:37 +0400

    format/restructure service provider boot

--- src/SaasykitPeachpaymentsServiceProvider.php
+++ src/SaasykitPeachpaymentsServiceProvider.php
@@ -18,47 +18,35 @@ class SaasykitPeachpaymentsServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        // Publish configuration file
         $this->publishes([
             __DIR__ . '/config/peachpayments.php' => config_path('peachpayments.php'),
         ], 'config');
 
-        // Publish Peachpayments logo to the public images folder
         $this->publishes([
             __DIR__ . '/public/images/payment-providers/peachpayments.png' => public_path('images/payment-providers/peachpayments.png'),
-        ], 'public');
+        ], 'public-images');
 
-        // Publish the Peachpayments settings view
-        $this->publishes([
-            __DIR__ . '/resources/views/livewire/filament/peachpayments-settings.blade.php' =>
-                resource_path('views/livewire/filament/peachpayments-settings.blade.php'),
-        ], 'views');
-
-        // Publish the PaymentProviderResource settings view
-        $this->publishes([
-            __DIR__ . '/resources/views/filament/admin/resources/payment-provider-resource/pages/peachpayments-settings.blade.php' =>
-                resource_path('views/filament/admin/resources/payment-provider-resource/pages/peachpayments-settings.blade.php'),
-        ], 'views');
-
-        // Publish the Peachpayments controller to the application's Http controllers directory
         $this->publishes([
             __DIR__ . '/app/Http/Controllers/PaymentProviders/PeachpaymentsController.php' =>
                 app_path('Http/Controllers/PaymentProviders/PeachpaymentsController.php'),
-        ], 'controllers');
+        ], 'app-http-controllers-payment-providers');
 
-        // Publish the PeachpaymentSettings.php file
-        $this->publishes([
-            __DIR__ . '/app/Filament/Admin/Resources/PaymentProviderResource/Pages/PeachpaymentSettings.php' => app_path('Filament/Admin/Resources/PaymentProviderResource/Pages/PeachpaymentSettings.php'),
-        ], 'filament-pages');
-
-        // Publish the peachpayments-how-to.blade.php file
-        $this->publishes([
-            __DIR__ . '/resources/views/filament/admin/resources/payment-provider-resource/pages/partials/peachpayments-how-to.blade.php' => resource_path('views/filament/admin/resources/payment-provider-resource/pages/partials/peachpayments-how-to.blade.php'),
-        ], 'partials');
-
-        // Publish the PeachpaymentsSettings.php file
         $this->publishes([
             __DIR__ . '/app/Livewire/Filament/PeachpaymentsSettings.php' => app_path('Livewire/Filament/PeachpaymentsSettings.php'),
-        ], 'livewire-components');
+        ], 'app-livewire-filament');
+
+        $this->publishes([
+            __DIR__ . '/resources/views/livewire/filament/peachpayments-settings.blade.php' =>
+                resource_path('views/livewire/filament/peachpayments-settings.blade.php'),
+            __DIR__ . '/resources/views/filament/admin/resources/payment-provider-resource/pages/peachpayments-settings.blade.php' =>
+                resource_path('views/filament/admin/resources/payment-provider-resource/pages/peachpayments-settings.blade.php'),
+            __DIR__ . '/resources/views/filament/admin/resources/payment-provider-resource/pages/partials/peachpayments-how-to.blade.php' =>
+                resource_path('views/filament/admin/resources/payment-provider-resource/pages/partials/peachpayments-how-to.blade.php'),
+        ], 'resources-views');
+
+        $this->publishes([
+            __DIR__ . '/app/Filament/Admin/Resources/PaymentProviderResource/Pages/PeachpaymentSettings.php' =>
+                app_path('Filament/Admin/Resources/PaymentProviderResource/Pages/PeachpaymentSettings.php'),
+        ], 'app-filament-admin-resources-payment-provider-resource-pages');
     }
 }
