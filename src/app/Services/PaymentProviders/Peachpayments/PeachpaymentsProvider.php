47c1070417ac2bbbf55e255a580f94fb9cd3635f
Fabio <fabio@mymage.io>
2025-02-20 14:15:19 +0400

    onetime purchase redirect URL generation

--- src/app/Services/PaymentProviders/Peachpayments/PeachpaymentsProvider.php
+++ src/app/Services/PaymentProviders/Peachpayments/PeachpaymentsProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\PaymentProviders\PeachPayments;
 
+use Carbon\Carbon;
 use App\Models\Discount;
 use App\Models\OneTimeProduct;
 use App\Models\Order;
@@ -16,22 +17,31 @@ use App\Constants\PlanPriceTierConstants;
 use App\Constants\PlanPriceType;
 use App\Constants\PlanType;
 use App\Constants\SubscriptionType;
-use Carbon\Carbon;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
-use App\Filament\Dashboard\Resources\SubscriptionResource;
-use PeachPayments\Checkout\CheckoutAPI;
 use App\Services\CalculationManager;
 use App\Services\DiscountManager;
 use App\Services\OneTimeProductManager;
 use App\Services\PaymentProviders\PaymentProviderInterface;
 use App\Services\PlanManager;
 use App\Services\SubscriptionManager;
+use PeachPayments\Checkout\CheckoutAPI;
+use PeachPayments\Checkout\CheckoutOptions;
+use PeachPayments\Checkout\Currency;
+use PeachPayments\CheckoutClient;
+use App\Filament\Dashboard\Resources\SubscriptionResource;
+
 
 class PeachPaymentsProvider implements PaymentProviderInterface
 {
-    private $client;
+    public function __construct(
+        private SubscriptionManager $subscriptionManager,
+        private PlanManager $planManager,
+        private CalculationManager $calculationManager,
+        private DiscountManager $discountManager,
+        private OneTimeProductManager $oneTimeProductManager,
+    ) {}
 
     public function getSlug(): string
     {
@@ -50,17 +60,56 @@ class PeachPaymentsProvider implements PaymentProviderInterface
 
     public function createProductCheckoutRedirectLink(Order $order, ?Discount $discount = null): string
     {
-        // Implement the logic to create a product checkout redirect link
+        $orderAmount = floatval(number_format($order->total_amount_after_discount / 100, 2));
+        $currencyCode = $order->currency()->firstOrFail()->code;
+        $supportedCurrencies = [
+            Currency::ZAR,
+            Currency::USD,
+            Currency::KES,
+            Currency::MUR,
+            Currency::GBP,
+            Currency::EUR,
+        ];
+
+        try {
+            if (!in_array($currencyCode, $supportedCurrencies)) {
+                throw new \Exception('Unsupported currency: ' . $currencyCode);
+            }
+
+            $client = new CheckoutClient(config('services.peachpayments.entity_id'), config('services.peachpayments.secret_token'));
+            if (!app()->environment('production')) {
+                $client->enableTestMode();
+            }
+
+            $options = new CheckoutOptions($order->id, $currencyCode, $orderAmount, 'https://httpbin.org/post'); //TODO replace with actual URL
+            $response = $client->checkout->initiateSession($options, url('/'));
+
+            if ($response->code == 201) {
+                return $response->body->redirectUrl;
+            }
+            else {
+                dd($response->body);
+                throw new \Exception($response->body->message);
+            }
+        }
+        catch (\Exception $e) {
+            Log::error('Failed to create Peach Payments checkout session: '.$e->getMessage());
+            throw new \Exception('Failed to create Peach Payments checkout redirect link: '.$e->getMessage());
+        }
+
+
     }
 
     public function initSubscriptionCheckout(Plan $plan, Subscription $subscription, ?Discount $discount = null): array
     {
         // Implement the logic to initialize a subscription checkout
+        return [];
     }
 
     public function initProductCheckout(Order $order, ?Discount $discount = null): array
     {
         // Implement the logic to initialize a product checkout
+        return [];
     }
 
     public function isRedirectProvider(): bool
